38bf0f83e973c28f482b51d20e1b9b1e57beba75
Marcus Mosley <marcus.mosley1993@gmail.com>
2024-10-29 15:40:30 -0500

    movie_year.rb

--- movie_year.rb
+++ movie_year.rb
@@ -4,3 +4,10 @@ puts movies[:gladiator]
 puts movies[:titanic]
 puts movies[:avengers]
 puts movies[:batman]
+
+dates = [2000, 1997, 2012, 2008]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
