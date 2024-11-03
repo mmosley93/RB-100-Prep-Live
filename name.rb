7186c18c0ab1b0113a8288a775cb71dfc9a1733b
Marcus Mosley <marcus.mosley1993@gmail.com>
2024-11-03 11:18:05 -0600

    name.rb

--- name.rb
+++ name.rb
@@ -1,6 +1,8 @@
-puts "Hi, what is your name?"
-name = gets.chomp
-puts "Nice to meet you #{name}."
-10.times do
-  puts name
-end
+puts "Hi, what is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Nice to meet you #{first_name} #{last_name}."
+# 10.times do
+#  puts name
+#end
